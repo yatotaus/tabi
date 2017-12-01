e6304ab1870ae49a1855e6ecd44b9d7a228812d6
yato <yatotaus@gmail.com>
2017-12-01 15:38:51 -0300

    agrega los include de navbar y footer, modificaciones en la vista de mapa_mundi.html

--- scripts/mapa_mundi.js
+++ scripts/mapa_mundi.js
@@ -1,17 +1,29 @@
 var sample_data = [
-  {"value": 2315987123, "country": "nausa", "name": "United States"},
-  {"value": 38157121349, "country": "aschn", "name": "China"},
-  {"value": 21891735098, "country": "euesp", "name": "Spain"},
-  {"value": 9807134982, "country": "sabra", "name": "Brazil"}
+  {"Cantidad de pobres a nivel nacional %": 42157121349, "country": "asafg", "name": "Afganistan"},
+  {"Cantidad de pobres a nivel nacional %": 22157121349, "country": "asind", "name": "India"},
+  {"Cantidad de pobres a nivel nacional %": 22157121349, "country": "askhm", "name": "Camboya"},
+
+  {"Cantidad de pobres a nivel nacional %": 58807134982, "country": "cahti", "name": "Haití"},
+  {"Cantidad de pobres a nivel nacional %": 28807134982, "country": "sacol", "name": "Colombia"},
+  {"Cantidad de pobres a nivel nacional %": 33307134982, "country": "saven", "name": "Venezuela"},
+    
+  {"Cantidad de pobres a nivel nacional %": 76157121349, "country": "afner", "name": "Niger"},
+  {"Cantidad de pobres a nivel nacional %": 66157121349, "country": "afcod", "name": "Rep. Democrática del Congo"},
+  {"Cantidad de pobres a nivel nacional %": 50157121349, "country": "afmli", "name": "Mali"},
+
+  {"Cantidad de pobres a nivel nacional %": 34157121349, "country": "ocfji", "name": "Fiyi"},
+  {"Cantidad de pobres a nivel nacional %": 12157121349, "country": "ocslb", "name": "Islas Salomon"},
+  {"Cantidad de pobres a nivel nacional %": 39157121349, "country": "ocpng", "name": "Papúa Nueva Guinea"}
 ]
-// instantiate d3plus
+
+  // instantiate d3plus
 var visualization = d3plus.viz()
-  .container("#viz")        
+  .container("#viz")        // container DIV to hold the visualization
   .data(sample_data)        
   .coords("http://d3plus.org/topojson/countries.json") // pass topojson coordinates
-  .type("geo_map")          
-  .id("country")            
+  .type("geo_map")          // visualization type
+  .id("country")            // key for which our data is unique on
   .text("name")             
-  .color("value")           
+  .color("Cantidad de pobres a nivel nacional %")           
   .tooltip("value")         
   .draw()                   
